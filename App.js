aef3426495cb091cea51f8589b9d73c39339e7a6
Carlos Craig <carlos.craig09@gmail.com>
2023-03-06 15:48:28 -0800

    App init changes

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Providers from "./navigation";
-import AppLoading from "expo-app-loading";
+import * as SplashScreen from "expo-splash-screen";
 import * as Font from "expo-font";
 // import { createStore, combineReducers, applyMiddleware } from "redux";
 // import { Provider } from "react-redux";
@@ -27,17 +27,40 @@ const fetchFonts = async () => {
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
 
-  if (!fontLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onError={console.warn}
-        onFinish={() => {
+  useEffect(() => {
+    if (!fontLoaded) {
+      async function prepare() {
+        try {
+          console.log("started");
+          // Keep the splash screen visible while we fetch resources
+          await SplashScreen.preventAutoHideAsync();
+          // Pre-load fonts, make any API calls you need to do here
+          await fetchFonts();
+        } catch (e) {
+          console.warn(e);
+        } finally {
+          console.log("loaded");
+          // Tell the application to render
           setFontLoaded(true);
-        }}
-      />
-    );
-  }
+        }
+      }
+
+      prepare();
+    }
+    if (fontLoaded) {
+      async function onLayoutRootView() {
+        // This tells the splash screen to hide immediately! If we call this after
+        // `setAppIsReady`, then we may see a blank screen while the app is
+        // loading its initial state and rendering its first pixels. So instead,
+        // we hide the splash screen once we know the root view has already
+        // performed layout.
+        await SplashScreen.hideAsync();
+      }
+
+      onLayoutRootView();
+    }
+  }, [fontLoaded]);
+
   return (
     // <Provider>
     <Providers />
